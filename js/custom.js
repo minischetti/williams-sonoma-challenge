c70ed07f72301fac5dcf70e4f46abe30c7623130
minischetti <domminischetti@gmail.com>
2017-02-20 18:16:11 -0500

    Change collapsible section method to HTML inputs

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+// var view = {
+//   displayTodos: function() {
+//     section = document.getElementsByClassName('section');
+//     section.forEach( function(todo, position) {
+//       section.appendChild('li');
+//     }, this);
+//   },
+//   setUpEventListeners: function () {
+//     // Set variable for content div
+//     var content = document.getElementById('content');
+//     // Listen for clicks on entire div
+//     content.addEventListener('click', function(event) {
+//       var elementClicked = event.target;
+//       // Set variable for collapsible containers
+//       container = document.getElementsByClassName('container');
+//       arrow = document.getElementsByClassName('arrow');
+//       // Toggle collapsed class on a per-container basis
+//         for (var i = 0; i < container.length; i++) {
+//           if (elementClicked.className === 'toggleSection', this) {
+//             container[i].classList.toggle('collapsed');
+//           }
+//         };
+//       console.log(elementClicked);
+//     });
+//   }
+// };
+//
+// view.setUpEventListeners();
