bf8e8f47aaa11d82e44ce138c8645b7847df52b0
minischetti <domminischetti@gmail.com>
2017-02-20 21:03:50 -0500

    Create product images dynamically, Change hero image based on selected product image

--- js/custom.js
+++ js/custom.js
@@ -36,3 +36,44 @@ currentQuantity.addEventListener('input', function()
 {
     document.getElementById('price').innerHTML = '$' + Number(19.95 * currentQuantity.value).toFixed(2);
 });
+
+var products = ["a", "b", "c", "d"];
+
+// Create Product Images
+function createImages() {
+  for (i = 0; i < products.length; i++) {
+    var productImage = document.createElement('img');
+    var imageContainer = document.getElementById('imageContainer');
+    imageContainer.appendChild(productImage);
+    productImage.id = products[i];
+    productImage.className = 'productImage';
+    productImage.src = 'assets/' + products[i] + 'Small.jpg';
+  }
+}
+
+function setUpEventListeners() {
+  // Set variable for content div
+  var imageContainer = document.getElementById('imageContainer');
+  // Listen for clicks on entire div
+  imageContainer.addEventListener('click', function(event) {
+    var elementClicked = event.target;
+    // Toggle collapsed class on a per-container basis
+    for (var i = 0; i < products.length; i++) {
+      if (elementClicked.className === 'productImage', this) {
+        document.getElementById("heroImage").src='assets/' + elementClicked.id + 'Large.jpg';
+      }
+    };
+    console.log(elementClicked);
+  });
+}
+// var view = {
+//   displayTodos: function() {
+//     section = document.getElementsByClassName('section');
+//     section.forEach( function(todo, position) {
+//       section.appendChild('li');
+//     }, this);
+//   },
+// };
+
+createImages();
+setUpEventListeners();
